5f8eba54f11eb331c4b20972bb85d76f03cbdcef
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-01-29 23:06:50 -0500

    Added Password generation mechanism to the encryption file

--- encrypt.go
+++ encrypt.go
@@ -4,9 +4,35 @@ import (
 	"io/ioutil"
 	"fmt"
 	"EncryptedDeploy/filecrypto"
+	"encoding/base64"
+	"os"
+	"log"
+	"crypto/rand"
 )
 
+func generatePassword() {
+	key := make([]byte, 32)
+
+	_, err := rand.Read(key)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(key)
+	keyAsString := base64.StdEncoding.EncodeToString(key)
+
+	file, err := os.Create("pass.txt")
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+
+	fmt.Fprintf(file, keyAsString)
+}
+
 func main() {
+
+	generatePassword()
+
 	b, err := ioutil.ReadFile("cred.txt")
 	if err != nil {
 		fmt.Print(err)
@@ -21,8 +47,5 @@ func main() {
 
 	fmt.Println(ciphertext)
 
-	ioutil.WriteFile("encrypted-credentials.txt", ciphertext, 0644)
-	//fmt.Println(b)
-	//str := string(b)
-	//fmt.Println(str)
+	ioutil.WriteFile("encrypted.txt", ciphertext, 0644)
 }
