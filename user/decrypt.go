7abb345dd5a805c893b593dbfd417a8e2fc98df6
Manikandan Sundararajan <mtiruman@buffalo.edu>
2018-02-01 17:29:21 -0500

    Trying out different ways of running the powershell scripts

--- user/decrypt.go
+++ user/decrypt.go
@@ -3,17 +3,41 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
-	"io/ioutil"
-	"os/exec"
 	"strings"
+	// "os"
+	// "os/exec"
+
+	ps "github.com/gorillalabs/go-powershell"
+	"github.com/gorillalabs/go-powershell/backend"
 
 	"github.com/tsmanikandan/EncryptedDeploy/crypto"
 )
 
+func powershellScriptGeneration(creds []string, exeName string) []string {
+	cmdList := []string{
+		"$usr = " + creds[0],
+		"Write-Host $usr",
+		"$passwd = " + creds[1],
+		"Write-Host $passwd",
+		"$credentials = New-Object System.Management.Automation.PSCredential -ArgumentList @($usr,(ConvertTo-SecureString -String $passwd -AsPlainText -Force))",
+		"Start-Process powershell -Credential $credentials -ArgumentList '-noprofile -command &{Start-Process .\\" + exeName + " -verb runas}'",
+	}
+	return cmdList
+}
+
 func main() {
-	b, err := ioutil.ReadFile("encrypted.txt")
+
+	dataAssets := AssetNames()
+	// RestoreAsset(".", "sample.ps1")
+	RestoreAsset(".", dataAssets[0])
+	// b, err := ioutil.ReadFile("encrypted.txt")
+	// if err != nil {
+	// 	fmt.Print(err)
+	// }
+
+	b, err := Asset(dataAssets[1])
 	if err != nil {
-		fmt.Print(err)
+		fmt.Println(err)
 	}
 
 	var passString string
@@ -32,13 +56,35 @@ func main() {
 	}
 
 	decipheredtext, _ := crypto.Open(passBytes, b)
-	credsForPowershell := string(decipheredtext)
-	creds := strings.Split(string(decipheredtext), " ")
-	uname, pwd := creds[0], creds[1]
-	fmt.Println(uname, pwd)
+	credPair := strings.Split(string(decipheredtext), " ")
 
-	credsForPowershell = ".\\sample.ps1 " + credsForPowershell
+	fmt.Println("Decrypted Credentials - ", credPair)
 
-	out, _ := exec.Command("powershell", credsForPowershell).Output()
-	fmt.Println(out)
+	scrpt := powershellScriptGeneration(credPair, dataAssets[0])
+
+	back := &backend.Local{}
+
+	shell, err := ps.New(back)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer shell.Exit()
+
+	stdout, stderr, err := shell.Execute(strings.Join(scrpt, "\n"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(stdout, stderr)
+
+	// fmt.Println(stdout, stderr)
+
+	// credsForPowershell := ".\\sample.ps1 " + string(decipheredtext)
+	// fmt.Println(credsForPowershell)
+
+	// cmd := exec.Command("powershell", credsForPowershell)
+	// fmt.Println("Running command and waiting for it to finish")
+	// err = cmd.Run()
+	// fmt.Println("Command finished with error: ", err)
+	// os.Remove("sample.ps1")
+	// os.Remove("wintest.exe")
 }
